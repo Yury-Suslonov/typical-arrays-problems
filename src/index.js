f0be077e12c1c478fbc9fec11085fd7ef248f50d
Yury-Suslonov <79038563+Yury-Suslonov@users.noreply.github.com>
2021-02-27 16:46:22 +0300

    feat: edit functions body

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array==undefined) return 0;
+  if (array.length==0) return 0;
+  let minimum=array[0];
+  for (let i=1;i<array.length;i++)
+  {
+      if (array[i]<minimum)
+      {
+          minimum=array[i];
+      }
+  }
+  return minimum;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array==undefined) return 0;
+  if (array.length==0) return 0;
+  let maximum=array[0];
+  for (let i=1;i<array.length;i++)
+  {
+      if (array[i]>maximum)
+      {
+          maximum=array[i];
+      }
+  }
+  return maximum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array==undefined) return 0;
+  if (array.length==0) return 0;
+  let sum=0;
+  for (let i=0;i<array.length;i++)
+  {
+      sum+=array[i]
+  }
+  return sum/array.length;
 }
